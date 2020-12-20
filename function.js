e36ac69db22eb5d69c19a0ce399e538a5b31d350
Mariano Víctor <67609481+VictorTigris@users.noreply.github.com>
2020-12-20 16:36:25 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+let bata, orin, dent, camp;
+bata = document.getElementById("shopping__1");
+orin = document.getElementById("shopping__2");
+dent = document.getElementById("shopping__3");
+camp = document.getElementById("shopping__4");
+function addChart(x) {
+  if (x == 1) {
+    console.log("bata");
+  } else if (x == 2) {
+    console.log("orinal");
+  } else if (x == 3) {
+    console.log("dientes");
+  } else if (x == 4) {
+    console.log("campana");
+  }
+}
